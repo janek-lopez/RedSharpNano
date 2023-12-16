384085dae527e11f868ccf1948ed41c54cd548cd
Janek López <20077043+janek-lopez@users.noreply.github.com>
2023-12-16 21:25:22 +0100

    Minimal refactoring and better naming of some variables

--- src/RedSharpNano/Resp2Client.cs
+++ src/RedSharpNano/Resp2Client.cs
@@ -9,8 +9,8 @@ public class Resp2Client : IDisposable
     private TcpClient _tcpClient;
     private NetworkStream _stream;
     private StreamReader _reader;
-    private List<string> _commandBuffer = new List<string>();
-    private bool _pipelineStarted = false;
+    private List<string> _pipelineCommandBuffer = new List<string>();
+    private bool _isPipelineActive = false;
     private bool _disposed = false;
 
     public Resp2Client(string server = "localhost", int port = 6379)
@@ -22,37 +22,32 @@ public class Resp2Client : IDisposable
 
     public List<object> Pipeline(Action<Resp2Client> pipelineActions)
     {
-        StartPipeline();
+        _pipelineCommandBuffer.Clear();
+        _isPipelineActive = true;
         pipelineActions(this);
         return ExecutePipeline();
     }
-
-    private void StartPipeline()
-    {
-        _commandBuffer.Clear();
-        _pipelineStarted = true;
-    }
-
+    
     private List<object> ExecutePipeline()
     {
-        var pipelinedCommand = string.Join("", _commandBuffer);
+        var pipelinedCommand = string.Join("", _pipelineCommandBuffer);
         _stream.Write(Encoding.UTF8.GetBytes(pipelinedCommand), 0, pipelinedCommand.Length);
         var responses = new List<object>();
 
-        for (int i = 0; i < _commandBuffer.Count; i++)
+        for (int i = 0; i < _pipelineCommandBuffer.Count; i++)
             responses.Add(ParseResponse());
 
-        _pipelineStarted = false;
-        _commandBuffer.Clear();
+        _isPipelineActive = false;
+        _pipelineCommandBuffer.Clear();
         return responses;
     }
 
     public object Call(string arg)
     {
-        var args = Regex.
-            Matches(arg, @"(?<match>\w+)|\""(?<match>[\w\s]*)""").
-            Select(m => m.Groups["match"].Value).
-            ToArray();
+        var args = Regex
+            .Matches(arg, @"(?<match>\w+)|\""(?<match>[\w\s]*)""")
+            .Select(m => m.Groups["match"].Value)
+            .ToArray(); //https://stackoverflow.com/questions/554013/regular-expression-to-split-on-spaces-unless-in-quotes
 
         return Call(args[0], args[1..]);
     }
@@ -61,13 +56,13 @@ public class Resp2Client : IDisposable
     {
         var cmd = PrepareCommand(method, args);
 
-        if (!_pipelineStarted)
+        if (!_isPipelineActive)
         {
             _tcpClient.GetStream().Write(Encoding.UTF8.GetBytes(cmd), 0, cmd.Length);
             return ParseResponse();
         }
 
-        _commandBuffer.Add(cmd);
+        _pipelineCommandBuffer.Add(cmd);
         return null;
     }
 
